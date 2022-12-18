27d267c1b6a80a5f6bb9b849f7e26e09db0fb9a6
Vinicius Heule <viniciusheule@gmail.com>
2022-12-18 17:23:58 -0300

    Creat Lesson, Content, Task and seed

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -13,7 +13,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 3);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 
-
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://www.publicdomainpictures.net/pictures/270000/nahled/course-classroom.jpg', 'https://www.open.edu/openlearn/pluginfile.php/768044/tool_ocwmanage/image/0/b716_2_cover_image_1.jpg');
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', TIMESTAMP WITH TIME ZONE '2020-10-13T20:50:07.12345Z', 1);
@@ -29,3 +28,18 @@ INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prer
 
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2022-07-13T20:50:07.12345Z', null, true, false);
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2022-07-13T20:50:07.12345Z', null, true, false);
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 1 do Cap. 1', 1, 1);
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 2 do Cap. 1', 2, 1);
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 3 do Cap. 1', 3, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (1, 'Material de apoio: teste', 'https://www.youtube.com/watch?v=0_e9Egeyk2E');
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (2, '', 'https://www.youtube.com/watch?v=0_e9Egeyk2E');
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (3, '', 'https://www.youtube.com/watch?v=0_e9Egeyk2E');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Tarefa do Cap. 1', 4, 1);
+INSERT INTO tb_task (id, description, question_Count, approval_Count, weight, due_Date) VALUES (4, 'Tarefa do Cap. 1', 10, 8, 1.0, TIMESTAMP WITH TIME ZONE '2022-07-30T20:50:07.12345Z');
+
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1, 1, 1);
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (2, 1, 1);
+
+
